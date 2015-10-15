c24fa634e50dcb35af0e16d2ca1ac0f355d68773
wwselleck <wwselleck@gmail.com>
2015-10-14 23:48:15 -0500

    Config things

--- python/main.js
+++ python/main.js
@@ -1,10 +1,13 @@
 from lib.logger import Logger
 from lib.winniebot import WinnieBot
-
-def run():
-  logger = Logger()
-  logger.logSmall("Hello")
+import praw
+import yaml
 
 if __name__ == "__main__":
+    with open("./config.yaml", "r") as f:
+      config = yaml.safe_load(f)
+    print(config)
+    user_agent = "WinnieBot v{:s} by {:s} [{:s}]".format(config['version'], config['author'], config['email'])
+    r = praw.Reddit(user_agent=user_agent)
     bot = WinnieBot(Logger())
     bot.run()
