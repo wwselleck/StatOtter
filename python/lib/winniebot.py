6ab4b62204da7a69f5fc8940810f967b1bdf6204
wwselleck <wwselleck@gmail.com>
2015-10-15 21:07:25 -0500

    General dev

--- python/lib/winniebot.py
+++ python/lib/winniebot.py
@@ -1,9 +1,22 @@
 from time import sleep
+from pprint import pprint
+
 class WinnieBot:
-    def __init__(self, logger):
+    def __init__(self, reddit, logger, options):
+        self.reddit = reddit
         self.logger = logger
+        self.options = options
 
     def run(self):
         while True:
             self.logger.logMedium("Running...")
+            recent_comments = self.reddit.get_comments(self.options['subreddit'])
+            for comment in recent_comments:
+                if "WinnieBot!" in comment.body:
+                    self.logger.logMedium('Comment Match Found!')
+                    pprint(vars(comment))
+                    self.process_comment(comment)
             sleep(2)
+
+    def process_comment(comment):
+        #todo
--- python/main.js
+++ python/main.js
@@ -6,8 +6,9 @@ import yaml
 if __name__ == "__main__":
     with open("./config.yaml", "r") as f:
       config = yaml.safe_load(f)
+
     print(config)
     user_agent = "WinnieBot v{:s} by {:s} [{:s}]".format(config['version'], config['author'], config['email'])
     r = praw.Reddit(user_agent=user_agent)
-    bot = WinnieBot(Logger())
+    bot = WinnieBot(r, Logger(), {"subreddit": config['target_subreddit']})
     bot.run()
